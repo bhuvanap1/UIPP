58998731cdf9f875e3d1f2e79ff9f865cdda78aa
bhuvanap1 <bhuvanasaikrishna@gmail.com>
2022-06-26 12:54:14 -0400

    Setting up react

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Going to build front end
+      </header>
+    </div>
+  );
+}
+
+export default App;
